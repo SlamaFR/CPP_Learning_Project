c172ac99bc47f7b0e7e86a826d20e82b9fe00cbb
Irwin Madet <irwin.madet@gmail.com>
2022-03-18 17:06:54 +0100

    Task 1.2.B

--- src/aircraft_factory.hpp
+++ src/aircraft_factory.hpp
@@ -12,6 +12,7 @@ private:
     const std::string airlines[8]          = { "AF", "LH", "EY", "DL", "KL", "BA", "AY", "EY" };
     const static size_t NUM_AIRCRAFT_TYPES = 3;
     AircraftType* aircraft_types[NUM_AIRCRAFT_TYPES] {};
+    std::set<std::string> existingFlightNumber;
 
     // note: textures have to be initialized after initializing glut, so we'll need
     // our own init here
@@ -26,10 +27,16 @@ private:
     {
         assert(airport); // make sure the airport is initialized before creating aircraft
 
-        const std::string flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+        std::string flight_number;
+        do {
+            flight_number = airlines[std::rand() % 8] + std::to_string(1000 + (rand() % 9000));
+        }
+        while (existingFlightNumber.find(flight_number) != existingFlightNumber.end());
         const float angle       = (rand() % 1000) * 2 * 3.141592f / 1000.f; // random angle between 0 and 2pi
         const Point3D start     = Point3D { std::sin(angle), std::cos(angle), 0 } * 3 + Point3D { 0, 0, 2 };
         const Point3D direction = (-start).normalize();
+
+        existingFlightNumber.emplace(flight_number);
 
         return std::make_unique<Aircraft>(type, flight_number, start, direction, airport->get_tower());
     }
